ccd50958c758fe721fcdc3175157477b0da78070
Alvin(Xinyao) Sun <lucklyric@gmaio.com>
2018-11-06 07:54:25 +0800

    :q

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git pull
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .vimrc ~/ 
 cp .tmux.conf ~/
